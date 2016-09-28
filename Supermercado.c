a54f03bfd7ad32e76d14aa31ac4694b9d97a4a13
root <root@localhost.localdomain>
2016-09-28 12:50:00 -0300

    quase finalizado faltando corrigir alguns bugs

--- Supermercado.c
+++ Supermercado.c
@@ -23,6 +23,7 @@ void inicializa_vendas();
 	
 int banco = 0;
 int espera[3];
+int vendas[3][VMAX];
 
 int *shm;
 sem_t fila_espera;
@@ -52,15 +53,21 @@ int main(){
 	
 	
 	pid_filho = fork();		
-	
+	inicializa_vendas(&vendas);
+	sem_init(&mutex, 0, 1);
+	sem_init(&fila_espera, 0, 0);
+
+
+
+
 	if(pid_filho == 0){
-		int vendas[3][VMAX];
-		inicializa_vendas(&vendas);
+		//int vendas[3][VMAX];
+		
 		
 		while(1){
 			while(shm[0] == 0);	
 			for(i = 0; i < 3; i++)
-				if(shm[0] == vendas [i][0] && vendas[i][2] < VMAX){
+				if(shm[0] == vendas [i][0] && vendas[i][2] < VMAX ){
 					vendas[i][vendas[i][2]] = shm[1];	// ultima posicao escrita do vetor
 					vendas[i][1] += shm[1];
 					banco += shm[1];
@@ -74,16 +81,15 @@ int main(){
 		
 		
 	}else if (pid_filho != 0){
-		sem_init(&mutex, 0, 1);
-		sem_init(&fila_espera, 0, 0);
+		
 		
 		pthread_t caixa1_t, caixa2_t, caixa3_t, gerenciador;
 
 		pthread_create(&gerenciador, NULL, (void *) gerenciador_vendas, NULL);
-		pthread_create(&caixa1_t, NULL, (void *) caixa, NULL);
-		pthread_create(&caixa2_t, NULL, (void *) caixa, NULL);
-		pthread_create(&caixa3_t, NULL, (void *) caixa, NULL);
-		pthread_join(gerenciador_vendas, NULL);
+		pthread_create(&caixa1_t, NULL, (void *) caixa, (void *) C1);
+		pthread_create(&caixa2_t, NULL, (void *) caixa, (void *) C2);
+		pthread_create(&caixa3_t, NULL, (void *) caixa, (void *) C3);
+		pthread_join(gerenciador, NULL);
 		pthread_join(caixa1_t, NULL);
 		pthread_join(caixa2_t, NULL);
 		pthread_join(caixa3_t, NULL);
@@ -103,41 +109,45 @@ int main(){
 
 
 void caixa (void *n){
+	int n_caixa = (int ) n;
+	//printf("recebi um numero %d \n", n_caixa);
 	while(1){
-    int valor = rand() % 100;
-	int n_caixa = rand()%3 // obtem um numero aleatorio entre a quantidade de caixas
-    while(valor > 0){
-			sem_wait(&mutex);
-		if(sem_wait(&mutex) == 0)
-			shm[0]= vendas[n_caixa][0];
-			shm[1] = valor;
-			fprintf(stderr,"Caixa de %d gravou R$ %d\n",vendas[n_caixa][0], valor);
-			valor = 0;
-			sem_post(&mutex);
-		}else {
-			printf("Caixa %d esperara com valor: %d ", vendas[n_caixa], valor);
+	    	int valor = rand() % 100;
+		fprintf(stderr,"Caixa de %d produziu R$ %d\n",n_caixa, valor);
+	     	while(valor > 0){
+			while(espera[0] != 0);
+			if(sem_wait(&mutex) == 0){
+				espera[0]= n_caixa;
+				espera[1] = valor;
+				valor = 0;
+				sem_post(&mutex);
+			}else {
+				printf("Caixa %d esperara com valor: %d ", n_caixa, valor);
+			}
+			sleep(rand()%5);
 		}
-		sleep(rand()%4);
 	}
 }
 
 
 void gerenciador_vendas(void *n){
-	while(1){
 		while(1){
-		sem_wait(&fila_espera); //down fila
-		sem_wait(&mutex); //down mutex
-		espera[0]=0;
-		shm[0]= espera[1];
-		shm[1]= espera[2];
+		while(espera[0] == 0);
+		sem_wait(&mutex);
+		 //down mutex
+		printf("caixa %d passado prara p1 com valor %d\n", espera[0], espera[1]);
+		shm[0]= espera[0];
+		sleep(1);
+		shm[1]= espera[1];
+		espera[0]=0;	
 		sem_post(&mutex);		
 		
-	}
+		}
 	
 }
 
 
-void inicializa_vendas(int *vendas[3][VMAX]){
+void inicializa_vendas(){
 	
 	
 	
